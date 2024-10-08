95ac5beccf1b8c72f3dc44256730283dc50ce414
thaodanghoang <thaodh03@gmail.com>
2024-10-08 12:27:53 -0400

    made some changes

--- HW3 Design/wildlife_tracker/animal_management/animal.py
+++ HW3 Design/wildlife_tracker/animal_management/animal.py
@@ -1,6 +1,5 @@
 from typing import Any, Optional
 
-
 class Animal:
 
      def __init__(self,
--- HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
+++ HW3 Design/wildlife_tracker/habitat_management/habitat_manger.py
@@ -1,12 +1,19 @@
 from typing import Optional, List, Any
 
+
 from wildlife_tracker.habitat_management.habitat import Habitat
+from wildlife_tracker.animal_management.animal import Animal
 
 class HabitatManager:
     
     def __init__(self) -> None:
         habitats: dict[int, Habitat] = {}
 
+def assign_animals_to_habitat(habitat_id: int, animals: List[Animal]) -> None:
+    pass
+
+def assign_animals_to_habitat(animals: List[Animal]) -> None:
+    pass
 
 def create_habitat(habitat_id: int, geographic_area: str, size: int, environment_type: str) -> Habitat:
     pass
